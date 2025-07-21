3c4f9a538d00c627adac1d0756f37e02a0d04dd7
srikanth0116 <srikanthchittirajula.gmail.com>
2025-07-21 17:44:59 +0530

    changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+let highscore = 0;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -16,6 +16,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
     //when guess is too high
   } else if (secretNumber < guess) {
     if (score > 1) {
